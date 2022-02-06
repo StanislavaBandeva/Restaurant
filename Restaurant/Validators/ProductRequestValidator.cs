3b4c3ffe90a991ecacd29fddfbd3ba6d15e608b7
StanislavaBandeva <s.bandeva99@gmail.com>
2022-02-06 22:05:29 +0200

    added fluent validation for product and table

---
+++ Restaurant/Validators/ProductRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Restaurant.Models.Requests;
+
+namespace Restaurant.Host.Validators
+{
+    public class ProductRequestValidator : AbstractValidator<ProductRequest>
+    {
+        public ProductRequestValidator()
+        {
+            RuleFor(x => x.Price).NotEmpty().NotNull();
+            RuleFor(x => x.Name).NotNull().NotEmpty();
+        }
+    }
+}
---
+++ Restaurant/Validators/TableRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Restaurant.Models.Requests;
+
+namespace Restaurant.Host.Validators
+{
+    public class TableRequestValidator : AbstractValidator<TableRequest>
+    {
+        public TableRequestValidator()
+        {
+            RuleFor(x => x.Orders).NotEmpty().NotNull();
+        }
+    }
+}
